ef25e5c56d6bc1fbcbc47c176be7269c2e7114de
Bert Deterd <bertdeterd@outlook.com>
2018-12-14 16:12:23 +0100

    bootstrap

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './pages/app';
 import { Provider } from 'react-redux';
 import configureStore from './store/configure-store';
 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
 const store = configureStore();
 
 ReactDOM.render( <Provider store={store}><App/></Provider>, document.getElementById('root'));
